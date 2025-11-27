e92530885861701e4dceb1b195e61535ae684601
kimchanghyun <kch633@naver.com>
2025-11-27 17:38:42 +0900

    251127_Commit

--- Project_E/Assets/Scripts/Study.cs
+++ Project_E/Assets/Scripts/Study.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class Study : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public string say = "";
+    private string said = "";
+    string newsay = "Hello, World!";
+    public char text;
     void Start()
     {
-        
-        int a = 10;
-        Debug.Log(a);
-        Debug.Log("Hello World!");
-        
-        
+        Debug.Log(this.say);
+        Debug.Log(newsay);
+        Debug.Log(said);
+        Debug.Log(this.text);
     }
 
     // Update is called once per frame
