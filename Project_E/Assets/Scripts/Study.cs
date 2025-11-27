f13d2f9877bdfeae1950e595de46ed93b06388b3
kimchanghyun <kch633@naver.com>
2025-11-27 10:26:42 +0900

    [Add] HelloWorld

---
+++ Project_E/Assets/Scripts/Study.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        int a = 10;
+        Debug.Log(a);
+        Debug.Log("Hello World!");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
